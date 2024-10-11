e879366e0ba46ea09b922b1eef1c7e674abcc3d4
jhorman <78864961+Jhormanrios0@users.noreply.github.com>
2024-10-11 09:55:00 -0500

    awo

--- assets/js/tailwindConfig.js
+++ assets/js/tailwindConfig.js
@@ -4,28 +4,11 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        principalColor: "#1c92d2",
+        primaryColor: "#3e6e30",
+        secundaryColor: "#f9f9f9",
+        thirdColor: "#f4f4f4",
       },
-      height: {
-        400: "400px",
-        cardsHeight: "270px",
-        containerServices: "1235px",
-        heightNews: "1412px",
-        heightFirstNew: "700px",
-        "95%": "95%",
-      },
-      width: {
-        cardsWidth: "280px",
-        400: "400px",
-        639: "639px",
-        1250: "1450px",
-        containerCards: "1280px",
-        1576: "1576px",
-        "95%": "95%",
-      },
-      spacing: {
-        "20px": "20px",
-      },
+      
     },
   },
 };
